19e6b2d85761496bcb797786376e1416cfe905ca
vuong123s <khanhvuong123s@gmail.com>
2026-05-21 07:37:26 +0700

    feat(menu): Menu flow + settings panel + reset progress

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// Dieu khien menu, fade va reset tien trinh.
 public class Menu : MonoBehaviour
 {
 	public GameObject panel;
 	public GameObject settingCanvas;
 	public void LoadScene(string sceneName)
     {
+		// Chuyen scene co hieu ung fade.
 		StartCoroutine(FadeIn(sceneName));
     }
 
@@ -18,6 +20,7 @@ public class Menu : MonoBehaviour
 	}
 	IEnumerator FadeIn(string sceneName)
 	{
+		// Bat panel va doi mot chut truoc khi load.
 		panel.SetActive(true);
 		yield return new WaitForSeconds(0.5f);//yied tra ve trong vai giay. xem canh Mantia Dot Lourdes truoc khi bat dau canh Game
 		SceneManager.LoadScene(sceneName);//tai mot scene moi trong game
@@ -25,18 +28,21 @@ public class Menu : MonoBehaviour
 
 	public void ShowPanel(GameObject panel)
 	{
+		// Mo canvas setting va panel con.
         settingCanvas.SetActive(true);
 		panel.SetActive(true);
     }
 
 	public void ClosePanel(GameObject panel)
 	{
+		// Dong canvas setting va panel con.
 		settingCanvas.SetActive(false);
 		panel.SetActive(false);
 	}
 
 	public void ResetLevel()
 	{
+		// Xoa tien trinh luu.
 		PlayerPrefs.DeleteAll();
 	}
 
