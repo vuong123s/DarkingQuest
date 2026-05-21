dde92f98fcd33c8f20e7b06f695f459b1594e9d4
HubertPhung <phungnguyenhoaibo@gmail.com>
2026-05-22 06:51:08 +0700

    add level selection and unlock setup

--- Assets/Scripts/UnlockLevel.cs
+++ Assets/Scripts/UnlockLevel.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+[RequireComponent(typeof(Button))]
 public class UnlockLevel : MonoBehaviour
 {
     private Button button;
-    public GameObject chains;
-    public string sceneName;
+    [SerializeField] private GameObject chains;
+    [SerializeField] private string sceneName;
     void Start()
     {
         button = GetComponent<Button>();
-        if(PlayerPrefs.GetInt(sceneName,0)==1)//neu k co gia tri mac dinh bang va neu bang 1 thi mo khoa level
+        if (string.IsNullOrWhiteSpace(sceneName))
+        {
+            Debug.LogError($"{name}: sceneName is empty in UnlockLevel.");
+            return;
+        }
+
+        if (PlayerPrefs.GetInt(sceneName,0)==1)//neu k co gia tri mac dinh bang va neu bang 1 thi mo khoa level
         {
             button.interactable = true;//tuong tac duoc voi button
-            chains.SetActive(false);// an day xich
+            if (chains != null)
+            {
+                chains.SetActive(false);// an day xich
+            }
+            else
+            {
+                Debug.LogWarning($"{name}: chains is not assigned in UnlockLevel.");
+            }
         }    
     }
 }
